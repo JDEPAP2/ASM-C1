2ea2a28aa5aa9fd97dc631dcc5e8e04fe831ec8f
JDEPAP2 <josedavidescobarprada@gmail.com>
2023-03-01 09:47:05 -0500

    sound

--- src/main/Map.js
+++ src/main/Map.js
@@ -1,21 +1,36 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Dialog from "@mui/material/Dialog";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useNavigate } from "react-router-dom";
+import { ChevronLeft, ChevronRight, CurrencyRupee } from "@mui/icons-material";
 
 const Map = (props) => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const [currentPlace, setCurrentPlace] = useState(true);
+  const [currentAudio, setCurrentAudio] = useState();
   const { places, center } = props;
   const L = require("leaflet");
 
-  const myIcon = (name)=> {
+  const myIcon = (name) => {
     return L.icon({
-    iconUrl: require(`../media/icons/${name}`),
-    iconSize: [64, 88],
-    iconAnchor: [32, 64],
-  })};
+      iconUrl: require(`../media/icons/${name}`),
+      iconSize: [64, 88],
+      iconAnchor: [32, 64],
+    });
+  };
+
+  const play = (file) =>{
+    const audio = new Audio(require(`../media/audio/${file}`))
+    audio.loop = true;
+    return audio;
+  }
+
+  useEffect(()=>{
+    if(currentAudio){
+      currentAudio.play()
+    }
+  },[currentAudio])
 
   return (
     <>
@@ -25,7 +40,7 @@ const Map = (props) => {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {places &&
-          places.map((place) => {
+          places.map((place, i) => {
             return (
               <div>
                 <Marker
@@ -33,8 +48,9 @@ const Map = (props) => {
                   icon={myIcon(place.icon)}
                   eventHandlers={{
                     click: () => {
-                        setOpen(true)
-                        setCurrentPlace(place)
+                      setOpen(true);
+                      setCurrentPlace({ ...place, i: i });
+                      setCurrentAudio(play(place.audio))
                     },
                   }}
                 ></Marker>
@@ -43,23 +59,49 @@ const Map = (props) => {
           })}
       </MapContainer>
       {open && (
-                  <Dialog
-                    open={open}
-                    onClose={() => setOpen(false)}
-                    aria-labelledby="alert-dialog-title"
-                    aria-describedby="alert-dialog-description"
-                  >
-                    <div className="flex justify-between items-center">
-                      <div className="py-10">
-                        <h1 className="py-5 text-5xl text-center font-bold tracking-tight leading-7 md:leading-snug truncate">
-                          {currentPlace.name}
-                        </h1>
-                        <img src={require(`../media/principal/${currentPlace.main_pic}`)}></img>
-                        {/* {navigate(`/Place/${place.id}`)} */}
-                      </div>
-                    </div>
-            </Dialog>)
-        }
+        <Dialog
+          open={open}
+          onClose={() => {
+            setOpen(false)
+            currentAudio.pause()
+          }}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+          transitionDuration={800}
+        >
+          <div className="flex justify-between items-center">
+            <div className="pt-4 pb-1">
+              <h1 className="text-5xl text-center font-normal tracking-tight leading-7 md:leading-snug truncate">
+                {currentPlace.name}
+              </h1>
+              <img
+                src={require(`../media/principal/${currentPlace.main_pic}`)}
+              ></img>
+              {/* {navigate(`/Place/${place.id}`)} */}
+            </div>
+          </div>
+          <div className="pb-2 flex justify-center items-center ">
+            <ChevronLeft 
+              color="action" 
+              sx={{ fontSize: "5em" }}
+              onClick = {()=> {
+                let i = (currentPlace.i == 0)? places.length-1 : currentPlace.i-1;
+                setCurrentPlace({...places[i], i:i})
+                currentAudio.pause()
+                setCurrentAudio(play(places[i].audio))
+              }}/>
+            <ChevronRight 
+              color="action" 
+              sx={{ fontSize: "5em" }}
+              onClick = {()=> {
+                let i = (currentPlace.i == places.length-1)? 0 : currentPlace.i+1;
+                setCurrentPlace({...places[i], i:i})
+                currentAudio.pause()
+                setCurrentAudio(play(places[i].audio))
+              }}/>
+          </div>
+        </Dialog>
+      )}
     </>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'Quicksand-SemiBold' : ['Quicksand-SemiBold','sans-serif']
+      }
+    },
   },
   plugins: [],
 }
